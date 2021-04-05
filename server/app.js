9d2429199a8798c4d648c0c1a42a6820bc8c46f8
SurelyNotChase <you@example.com>
2021-04-04 23:01:12 -0400

    removed unused var

--- server/app.js
+++ server/app.js
@@ -31,8 +31,8 @@ const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
-app.use(bodyParser.urlencoded({ 
-  extended: true, 
+app.use(bodyParser.urlencoded({
+  extended: true,
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -42,8 +42,8 @@ app.use(cookieParser());
 router(app);
 
 app.listen(port, (err) => {
-  if (err) { 
+  if (err) {
     throw err;
-   }
+  }
   console.log(`Listening on port ${port}`);
 });
