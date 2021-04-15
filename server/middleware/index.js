cd40b8a073db5f6fbb119e1466a257f8ee2b2689
SurelyNotChase <you@example.com>
2021-04-14 21:00:20 -0400

    d

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -13,8 +13,6 @@ const requiresLogout = (req, res, next) => {
 };
 
 const requiresSecure = (req, res, next) => {
-
-
   if (req.headers['x-forwarded-proto'] !== 'https') {
     return res.redirect(`https://${req.hostname}${req.url}`);
   }
